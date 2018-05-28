6ef45e4bbcb26a80185b19fd4c27c20489e463e2
Cody Spate <codyspate@gmail.com>
2018-05-28 00:49:11 -0700

    push

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Logo from './ts-logo.png';
 
 const Header = ({ className }) => (
 	<div className="header">
-		<h1>Tim Spate</h1>
-		<h2>Full Stack Web Developer</h2>
-		<span>
-			<a href="mailto:contact@tspate.com">contact@tspate.com</a>
-		</span>
+		<div>
+			<h1>Tim Spate</h1>
+			<h2>Full Stack Web Developer</h2>
+			<span>
+				<a href="mailto:contact@tspate.com">contact@tspate.com</a>
+			</span>
+		</div>
+		<img className="logo" alt="Tim Spate Logo" src={Logo} />
 	</div>
 );
 
