ccae1f7bf97b596170dfa57b4a983097637783f6
Cody Spate <codyspate@gmail.com>
2018-06-27 19:16:04 -0700

    feat: added ee

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Header from './components/header/header';
 import Body from './components/body/body';
+import Temp from './components/temp-page/temp-page';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Header />
-				<Body />
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Header />
+					<Route path="/" exact component={Body} />
+					<Route path="/ee" exact component={Temp} />
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
---
+++ src/components/temp-page/temp-page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Temp = props => {
+	return (
+		<div>
+			<h2>This is a solution to all the windows issues you have been having Gage.</h2>
+			<p>
+				<a href="https://tinyurl.com/ybdqhbrm" target="_blank" rel="noopener noreferrer">
+					Never suffer again ->
+				</a>
+			</p>
+			{/* <p>
+				<Link to="/">Home</Link>
+			</p> */}
+		</div>
+	);
+};
+
+export default Temp;
